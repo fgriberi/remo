4bc51934ba82c2922bd3616efb202a012a919539
Franco Riberi <fgriberi@gmail.com>
2012-11-07 11:00:27 -0300

    Added remo.h file

---
+++ remo/remo.h
@@ -0,0 +1,32 @@
+/*
+    Copyright (C) 2012 Franco Riberi, FuDePAN
+
+    This file is part of the remo.
+
+    Remo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Remo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Remo. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef REMO_H
+#define REMO_H
+
+#include "MOP.h"
+#include "ICodonUsageModifier.h"
+#include "Definitions.h"
+#include "Exceptions.h"
+#include "OutputsGenerator.h"
+#include "TablesGenerator.h"
+#include "StatisticalControl.h"
+
+#endif  /* REMO_H */
