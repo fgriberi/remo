b77762c06b29c9ab2f10c35b87a2d82887c21e8c
Franco Riberi <fgriberi@gmail.com>
2013-03-19 18:20:41 -0300

    Update design

--- doc/design/highLevel/compile.sh
+++ doc/design/highLevel/compile.sh
@@ -1,16 +1,5 @@
-pdflatex ArquitectureResponsibilities.tex
-bibtex ArquitectureResponsibilities.aux 
-pdflatex ArquitectureResponsibilities.tex
-pdflatex ArquitectureResponsibilities.tex
-
-pdflatex docArquitecture.tex
-bibtex docArquitecture.aux 
-pdflatex docArquitecture.tex
-pdflatex docArquitecture.tex
-
-pdflatex diagrams.tex
-bibtex diagrams.aux 
-pdflatex diagrams.tex
+pdflatex ArchitectureResponsibilities.tex
+pdflatex docArchitecture.tex
 pdflatex diagrams.tex
 
 rm *.aux *.bbl *.blg *.log
