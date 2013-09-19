f994eb418dd5f99d5c5507b86898c5be0ffa8e1e
Franco Riberi <fgriberi@gmail.com>
2013-09-19 19:00:40 -0300

    Added header comments

--- gui/Main.cpp
+++ gui/Main.cpp
@@ -1,3 +1,33 @@
+/**
+ *  @file:      Main.cpp
+ *  @details    Gui main file.\n
+ *              System: R-emo \n
+ *              Language: C++\n
+ *
+ *  @author     Franco Riberi
+ *  @email      fgriberi AT gmail.com
+ *
+ *  @date       September 2013
+ *
+ * This file is part of R-emo.
+ *
+ * Copyright (C) 2013 - Franco Riberi, FuDePAN.
+ *
+ * R-emo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * R-emo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with R-emo. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "Application.h"
 
 int main(int argc, char* argv[])
@@ -7,4 +37,3 @@ int main(int argc, char* argv[])
     while (app.run());
     return 0;
 }
-
--- gui/MainWindow.h
+++ gui/MainWindow.h
@@ -1,3 +1,36 @@
+/*
+ * @file     MainWindow.h
+ * @brief    Provide the interface to Main window.
+ *
+ * @author   Franco Riberi
+ * @email    fgriberi AT gmail.com
+ *
+ * Contents: Source file for r-emo providing Main window interface.
+ *
+ * Language: C++
+ *
+ * @date      September 2013
+ *
+ * This file is part of r-emo.
+ *
+ * Copyright (C) 2013 - Franco Riberi, FuDePAN.
+ *
+ * r-emo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * r-emo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with r-emo. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
